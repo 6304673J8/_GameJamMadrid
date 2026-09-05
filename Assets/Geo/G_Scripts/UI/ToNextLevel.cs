ea21c275e34351ee5034d2687fbb964239c12e33
G30Ju <73468022+6304673J8@users.noreply.github.com>
2026-09-06 00:29:45 +0200

    Levels Implemented
    To test in Build

--- Assets/Geo/G_Scripts/UI/ToNextLevel.cs
+++ Assets/Geo/G_Scripts/UI/ToNextLevel.cs
@@ -3,17 +3,16 @@ using UnityEngine.SceneManagement;
 
 public class ToNextLevel : MonoBehaviour
 {
-    private int nextSceneToLoad;
-    [SerializeField] private GameObject _avoidReturn;
+    [SerializeField] private int nextSceneToLoad = 0;
 
     void Start()
     {
-        nextSceneToLoad = SceneManager.GetActiveScene().buildIndex + 1;
+        if (nextSceneToLoad == 0)
+            nextSceneToLoad = SceneManager.GetActiveScene().buildIndex + 1;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        _avoidReturn.SetActive(true);
         //SceneTransitionManager.singleton.GoToSceneAsync(nextSceneToLoad);
         TransitionManager.singleton.GoToSceneAsync(nextSceneToLoad);
     }
